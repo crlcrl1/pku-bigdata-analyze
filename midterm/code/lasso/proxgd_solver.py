a8aa8dfbe0277ff475ad8f9fbf85bcada188324f
crl <crl18039102576@126.com>
2025-03-21 10:10:03 +0800

    [feat] BB step size for prox gradient

--- midterm/code/lasso/proxgd_solver.py
+++ midterm/code/lasso/proxgd_solver.py
@@ -8,7 +8,7 @@ from util import lasso_loss, run_algorithm
 
 def prox_solver(A: NDArray, b: NDArray, mu: float) -> Tuple[float, NDArray, int]:
     m, n = A.shape
-    step_size = 1 / (np.linalg.norm(A, ord=2) ** 2)
+    step_size = 0.01
     mu_list = list(reversed([(5 ** i) * mu for i in range(6)]))
     last_idx = len(mu_list) - 1
     f_last = np.inf
@@ -18,16 +18,25 @@ def prox_solver(A: NDArray, b: NDArray, mu: float) -> Tuple[float, NDArray, int]
     ata = A.T @ A
     atb = A.T @ b
 
+    grad_last = np.zeros(n)
+    x_last = np.zeros(n)
+
     for k, mu in enumerate(mu_list):
         inner_iter = 0
         while True:
             total_iter += 1
             inner_iter += 1
-            if inner_iter > 150 and k != last_idx:
+            if inner_iter > 50 and k != last_idx:
                 break
 
             # apply grad step
             grad = ata @ x - atb
+            s = x - x_last
+            y = grad - grad_last
+            if total_iter > 1:
+                step_size = np.dot(s, s) / np.dot(s, y)
+            x_last = x.copy()
+            grad_last = grad
             x -= step_size * grad
 
             # apply prox
